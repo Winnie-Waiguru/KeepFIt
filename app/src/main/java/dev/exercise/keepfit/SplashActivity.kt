44770ba6b3938891a159f504b56a19bb93543bf5
Winnie-Waiguru <winniewaiguru@gmail.com>
2022-05-31 19:31:15 +0300

    first commit

---
+++ app/src/main/java/dev/exercise/keepfit/SplashActivity.kt
@@ -0,0 +1,15 @@
+package dev.exercise.keepfit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent= Intent(this, LogInActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
